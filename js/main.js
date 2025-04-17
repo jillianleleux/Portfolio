14b2ca0613bee1f1dbcbd8c23221bc68ccd37061
jillianleleux <jleleu9@lsu.edu>
2025-04-17 10:54:51 -0500

    experimenting

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
 // JS scripts placed here
+let currentPage = 1;
+const totalPages = 5; // Number of `.page` elements
+
+document.getElementById('nextBtn').addEventListener('click', () => {
+  if (currentPage <= totalPages) {
+    const page = document.getElementById(`page${currentPage}`);
+    page.style.transform = 'rotateY(-180deg)';
+    currentPage++;
+  }
+});
+
+document.getElementById('prevBtn').addEventListener('click', () => {
+  if (currentPage > 1) {
+    currentPage--;
+    const page = document.getElementById(`page${currentPage}`);
+    page.style.transform = 'rotateY(0deg)';
+  }
+});
 
 
 
